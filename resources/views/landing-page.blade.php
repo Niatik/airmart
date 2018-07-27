40005424a2b7cae622c938481803658ff1f279ea
Nikita <nikita.dragunov@gmail.com>
2018-07-27 22:26:46 +0600

    Create landing page with html markup.

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AIR MART интернет-магазин</title>
+    <link href="css/normalize.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="logo_contacts">
+        <div class="logo">
+            <img src="img/logo.png" alt="Логотип Air Mart" />
+        </div>
+        <div class="contacts">
+            <div class="eshop">Интернет-магазин</div>
+            <div class="call-img">
+                <img src="img/call-lime.svg"><span class="call red">Заказать звонок</span>
+            </div>
+            <div class="phones">
+                <span class="phone">тел.: </span><span class="number_phones">+7 775 114 13 26,</span><br>
+                <span class="number_phones">+7 727 247 71 28</span><br>
+            </div>
+        </div>
+    </div>
+    <div class="nav">
+        <ul>
+            <li>
+                <a href="#" class="no-underline">Каталог</a>
+            </li>
+            <li>
+                <a href="#" class="no-underline">Доставка и оплата</a>
+            </li>
+            <li>
+                <a href="#" class="no-underline">Сервис и ремонт</a>
+            </li>
+            <li>
+                <a href="#" class="no-underline">Отзывы</a>
+            </li>
+        </ul>
+    </div>
+</header>
+<div class="container">
+    <section class="offers">
+        <div class="offer_image1">
+            <img src="img/img02_0.png" alt="">
+            <span class="offer_text1">
+                AirMArt с <span class="red">БЕСПЛАТНОЙ</span> доставкой<br>
+                увлажнителей по Казахстану<br>
+                из г. Алматы.<br>
+                <span class="red mt-3 block">+ 1 год ГАРАНТИИ!</span>
+            </span>
+        </div>
+
+        <h2 class="offer3 red">АКЦИЯ - 20 % СКИДКИ <span>на все модели увлажнителей</span></h2>
+
+        <div class="offer_image2">
+            <img src="img/img01.png" alt="">
+            <span class="offer_text2">
+                Ультразвуковыми увлажнители воздуха  AirMArt<br>
+                предназначены для жилых комнат и офисов.<br>
+                Наши увлажнители помогают:<br>
+
+                <ul>
+                    <li>Заботиться о здоровье</li>
+                    <li>Устранять пыль и сохранять чистоту в доме</li>
+                    <li>Ухаживать за кожей лица и волосами</li>
+                    <li>Применять SPA процедуры</li>
+                    <li>Снимать стресс и расслабляться</li>
+                    <li>Увлажнять помещение</li>
+                </ul>
+            </span>
+        </div>
+
+        <!--<div id="form" class="float-right">
+            ОСТАВЬТЕ ЗАЯВКУ
+            и получите бесплатную
+            консультацию
+
+            Введите ваше имя
+
+            Ваш телефон
+
+            Ваш e-mail
+
+            Оставить заявку
+        </div>-->
+    </section>
+    <section class="advantages">
+        <h3 class="advantages-header">
+            ПРЕИМУЩЕСТВА
+        </h3>
+        <div class="advantages-content">
+            <div class="round-image">
+                <img src="img/img07.png"/>
+                <p>
+                    Дизайн с текстурой<br />
+                    дерева сделанный<br />
+                    из экопластика<br />
+                    <br />
+                    <br />
+                    <br />
+                </p>
+            </div>
+            <div class="round-image">
+                <img src="img/img09.png" />
+                <p>
+                    7 цветов подсветки.<br />
+                    Режимы: Плавная смена цветов/<br />
+                    фиксация цвета/без освещения<br />
+                    <br />
+                    <br />
+                    <br />
+                </p>
+            </div>
+            <div class="round-image">
+                <img src="img/img16.png" />
+                <p>
+                    Увлажняет, облегчает дыхание.<br />
+                    Забудьте о стянутости кожи,<br />
+                    пересохшем горле.<br />
+                    Поддерживайте влажность<br />
+                    в помещении.<br />
+                    <br />
+                </p>
+            </div>
+            <div class="round-image">
+                <img src="img/img14.png"/>
+                <p>
+                    С Функцией добавления аромамасел.<br />
+                    Добавляйте натуральные эфиные масла<br />
+                    для профилактики здоровья и просто<br />
+                    для релаксации. Они снимают усталость<br />
+                    и напряжение. Создают атмосферу уюта<br />
+                    и спокойствия.
+                </p>
+            </div>
+            <div class="round-image">
+                <img src="img/img11.png"/>
+                <p>
+                    Бесшумный. Ультразвуковой принцип<br />
+                    работы позволяет увлажнять помещение<br />
+                    практически бесшумно, спите спокойно.<br />
+                    Автоматическое отключение при полном<br />
+                    испарении воды.<br />
+                    <br />
+                </p>
+            </div>
+        </div>
+        <div class="bottom-advantages">
+
+
+        </div>
+    </section>
+    <section class="benefits">
+        <h3 class="benefits-header">
+            ВЫГОДЫ ОТ ПРИОБРЕТЕНИЯ УВЛАЖНИТЕЛЕЙ ОТ AIRMART
+        </h3>
+        <div class="benefits-content">
+            <div class="symbol-image">
+                <img src="img/delivery-lime.png" />
+                <p>
+                    БЕСПЛАТНАЯ БЫСТРАЯ<br />
+                    доставка во все<br />
+                    города Казахстана
+                </p>
+            </div>
+            <div class="symbol-image">
+                <img src="img/warranty.png" />
+                <p>
+                    ГАРАНТИЯ<br />
+                    12 месяцев<br />
+                    <br />
+                </p>
+            </div>
+            <div class="symbol-image">
+                <img src="img/nomber-one.png" />
+                <p>
+                    АМЕРИКАНСКИЙ<br />
+                    бренд AIRMART №1<br />
+                    в Казахстане
+                </p>
+            </div>
+            <div class="symbol-image">
+                <img src="img/clients.png" />
+                <p>
+                    Более 2000<br />
+                    ДОВОЛЬНЫХ<br />
+                    клиентов
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="subscription">
+        <h3 class="subscription-header">
+            ПОЛУЧИТЕ РЕЦЕПТЫ
+            АРОМАТЕРАПИИ
+        </h3>
+        <form class="subscription-form">
+            <input class="subscription-email" type="email" placeholder="Введите ваш e-mail">
+            <input class="subscription-button" type="submit" value="Подписаться">
+        </form>
+
+    </section>
+    <section class="warning">
+        <h3 class="red">ВНИМАНИЕ!</h3>
+        <div>
+            Рынки и торговые точки Казахстана заполонили дешевые копии
+            увлажнителей воздуха из Китая, которые быстро выходят из строя, ПЕРЕНАГРЕВАЮТСЯ И ЛОМАЮТСЯ.
+        </div>
+        <div>
+            Компания  AirMart предлагает качественного произвоства США ультразвукоые увлажнители воздуха.
+        </div>
+        <div>
+            Напоминаем, что оригинальные увлажнители воздуха AirMart с официальной гарантией
+            Вы можете купить только в нашем интернет-магазине на нашем сайте.
+        </div>
+    </section>
+</div>
+<footer class="footer">
+    <div class="symbol-footer">
+        <img src="img/check.png" />
+        <p>
+            Закажите товар на сайте или<br />
+            Получите консультацию по телефону<br />
+            <br />
+        </p>
+    </div>
+    <div class="arrow">
+        <img src="img/arrow.png">
+    </div>
+    <div class="symbol-footer">
+        <img src="img/call.png" />
+        <p>
+            Менеджер перезвонит<br />
+            для уточнения заказа<br />
+            <br />
+        </p>
+    </div>
+    <div class="arrow">
+        <img src="img/arrow.png">
+    </div>
+    <div class="symbol-footer">
+        <img src="img/delivery.png" />
+        <p>
+            Отправляем курьером<br />
+            Ваш увлажнитель в удобное<br />
+            для Вас время
+        </p>
+    </div>
+</footer>
+</body>
+</html>
