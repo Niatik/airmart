504d3723c57f77c7a2337a04c107e13522081280
Nikita <nikita.dragunov@gmail.com>
2018-07-27 22:52:16 +0600

    Add route for landing page.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+//$app->get('/', function () use ($app) {
+//    return $app->version();
+//});
+
 $app->get('/', function () use ($app) {
-    return $app->version();
+    return view('landing-page');
 });
