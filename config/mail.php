88086c4ca2c137fa8096ed778c700d29811a85fe
Nikita Dragunov <nikita.dragunov@gmail.com>
2018-07-30 15:25:04 +0600

    Set mail parameters.

--- config/mail.php
+++ config/mail.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'driver' => env('MAIL_DRIVER'),
-    'host' => env('MAIL_HOST'),
-    'port' => env('MAIL_PORT'),
+    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'host' => env('MAIL_HOST', 'energouchet.kz'),
+    'port' => env('MAIL_PORT', '25'),
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS'),
-        'name' => env('MAIL_FROM_NAME'),
+        'address' => env('MAIL_FROM_ADDRESS',  'info@energouchet.kz'),
+        'name' => env('MAIL_FROM_NAME', 'Служба ЮК-ДАСУ'),
     ],
-    'encryption' => env('MAIL_ENCRYPTION'),
-    'username' => env('MAIL_USERNAME'),
-    'password' => env('MAIL_PASSWORD'),
+    'encryption' => env('MAIL_ENCRYPTION', ''),
+    'username' => env('MAIL_USERNAME', 'info@energouchet.kz'),
+    'password' => env('MAIL_PASSWORD', 'ypOPYq872w'),
     'markdown' => [
         'theme' => 'default',
         'paths' => [
