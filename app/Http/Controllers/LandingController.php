5c58ac0765831a9773ed89ba3e6abfe528932048
Nikita Dragunov <nikita.dragunov@gmail.com>
2018-07-30 15:26:09 +0600

    Change "call" action in LandingController to send mail.

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail as Mail;
+use App\Mail\CallOrder as CallOrder;
 
 class LandingController extends Controller
 {
     public function call()
     {
-        return 0;
+        Mail::send(
+            new CallOrder(
+               'n.dragunov@energouchet.kz'
+            )
+        );
     }
 
     public function index()
